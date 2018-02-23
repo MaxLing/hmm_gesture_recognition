270caf22763af3d841d1f8939025ded412a3d760
Max LING <wudaoling7@gmail.com>
2018-02-22 21:57:01 -0500

    change all params to log space

--- hidden_markov_model.py
+++ hidden_markov_model.py
@@ -1,3 +1,6 @@
+'''
+all calculation is in log space
+'''
 import numpy as np
 from numpy import log
 from scipy.special import logsumexp
@@ -5,28 +8,34 @@ from scipy.special import logsumexp
 def hmm_train(trans, emiss, init, max_iter, tol, data):
     M, N = emiss.shape
     for gesture in data['gestures']:
-        A = np.copy(trans)
-        B = np.copy(emiss)
-        PI = np.copy(init)
+        A = np.log(trans)
+        B = np.log(emiss)
+        PI = np.log(init)
         obs_seqs = [data[key] for key in data.keys() if gesture in key] # multiple observation sequences
+        K = len(obs_seqs)
 
         for i in range(max_iter):
-            gamma = np.zeros(N)
-            xi = np.zeros((N, N))
-            log_likelihood = np.zeros(len(obs_seqs))
+            gamma_sum = np.zeros((1, N))
+            xi_sum = np.zeros((N, N))
+            obs_count = np.zeros((M,N))
+            log_likelihood = np.zeros(K)
 
             for idx, obs in enumerate(obs_seqs):
                 ## E-step
-                # params in log space: alpha, beta, P, gamma, xi
+                # computer statistics of hidden states
                 alpha, beta, P = forward_backward(obs, A, B, PI)
                 log_likelihood[idx] = P
                 gamma = smoothing(alpha, beta)
                 xi = pair_states(alpha, beta, A, B, obs)
 
-            mean_log_likelihood = np.mean(log_likelihood)
-            print(gesture, '\tIteration: ', i+1, '\tAverage log-likelihood: ', mean_log_likelihood)
+                for l in range(M):
+                    obs_count[l] += np.sum(gamma[obs==l], axis=0)
+
+            all_log_likelihood = np.sum(log_likelihood)
+            print(gesture, '\tIteration: ', i+1, '\t obs log-likelihood: ', all_log_likelihood)
 
             ## M-step
+            # reestimate parameters that best fit inferred distribution
 
 
 
@@ -40,10 +49,10 @@ def forward_backward(obs, A, B, PI):
     # Forward procedure
     # init
     alpha = np.zeros((T,N))
-    alpha[0] = log(PI)+log(B[obs[0]])
+    alpha[0] = PI + B[obs[0]]
     # induction
     for t in range(T-1):
-        alpha[t+1] = logsumexp(alpha[t]+log(A), axis=1) + log(B[obs[t+1]])
+        alpha[t+1] = logsumexp(alpha[t].reshape((1,-1)) + A, axis=1) + B[obs[t+1]]
     # termination, compute likelihood
     P = logsumexp(alpha[T-1])
 
@@ -53,20 +62,22 @@ def forward_backward(obs, A, B, PI):
     beta[T-1] = log(1)
     # induction
     for t in range(T-2, -1, -1):
-        beta[t] = logsumexp(beta[t+1]+log(A.T)+log(B[obs[t+1]]), axis=1)
+        beta[t] = logsumexp(beta[t+1].reshape((-1,1)) + A + B[obs[t + 1]].reshape((-1,1)), axis=0)
     # termination, for debug purpose
-    Q = logsumexp(beta[0]+log(PI))
-    # TODO: figure out P == Q
-    # print('forward log likelihood: ', P, '\tbackward log likelihood: ', Q)
+    Q = logsumexp(beta[0]+PI)
+    # TODO: figure out why P != Q
+    print('forward log likelihood: ', P, '\tbackward log likelihood: ', Q)
+
     return alpha, beta, P
 
 def smoothing(alpha, beta):
     T, N = alpha.shape
-    gamma = np.zeros((T,N))
-
-    for t in range(T):
-        gamma[t] = alpha[t] + beta[t]
-    gamma -= logsumexp(gamma, axis=1).reshape((-1,1)) # normalization
+    # gamma = np.zeros((T,N))
+    #
+    # for t in range(T):
+    #     gamma[t] = alpha[t] + beta[t]
+    gamma = alpha + beta
+    gamma -= logsumexp(gamma, axis=1).reshape((T,1)) # normalization
 
     return gamma
 
@@ -75,9 +86,9 @@ def pair_states(alpha, beta, A, B, obs):
     xi = np.zeros((N, N, T-1))
 
     for t in range(T-1):
-        xi[:, :, t] = alpha[t]+log(A)+log(B[obs[t+1]])+beta[t+1]
+        xi[:, :, t] = alpha[t].reshape((-1,1)) + A.T + B[obs[t+1]].reshape((1,-1)) + beta[t+1].reshape((1,-1))
     xi -= logsumexp(xi, axis=2).reshape((N,N,1)) # normalization
 
     return xi
 
-# def log_baum_welch(observations):
+# def baum_welch(observations):
